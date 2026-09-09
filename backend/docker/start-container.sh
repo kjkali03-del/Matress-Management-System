f1caf14b0081cbb44993754a8ca7239a0c304d5b
kjkali-dev <kjkali03@gmail.com>
2026-09-09 16:00:14 +0300

    Add Render Docker deployment infrastructure

---
+++ backend/docker/start-container.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eu
+
+: "${PORT:=10000}"
+
+envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views storage/logs bootstrap/cache
+chown -R www-data:www-data storage bootstrap/cache
+
+php artisan package:discover --ansi >/dev/null
+
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
